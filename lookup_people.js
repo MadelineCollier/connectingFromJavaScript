7d3d76bbcdc6dd30f0e08c216d5f0312d34bf256
Madeline Collier <madel@telus.net>
2017-07-18 22:08:46 +0000

    refactored out the printing of the results into functions

--- lookup_people.js
+++ lookup_people.js
@@ -10,6 +10,17 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
+
+const printPeople = function (result) {
+  console.log(`Found ${result.rows.length} person(s) by the name '${process.argv.slice(2)[0]}'`);
+};
+
+const printPerson = function (result) {
+  result.rows.forEach((result) => {
+    console.log(`-${result.id}: ${result.first_name} ${result.last_name}, born '${result.birthdate.toString().substring(4, 15)}' `);
+  });
+};
+
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
@@ -19,8 +30,8 @@ client.connect((err) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(`Found ${result.rows.length} person(s) by the name '${process.argv.slice(2)[0]}'`);
-    console.log(`-${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born '${result.rows[0].birthdate.toString().substring(4, 15)}' `);
+    printPeople(result);
+    printPerson(result);
     client.end();
   });
 });
